eed56892119746c2a549a32f335ef4b08f924ea3
hhacker1999 <harsh.sadh313@gmail.com>
2022-06-18 16:44:43 +0530

    initial neovim config

---
+++ lua/user/alpha.lua
@@ -0,0 +1,46 @@
+local status_ok, alpha = pcall(require, "alpha")
+if not status_ok then
+  return
+end
+
+local dashboard = require "alpha.themes.dashboard"
+dashboard.section.header.val = {
+    "                                                     ",
+    "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+    "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+    "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+    "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+    "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+    "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+    "                                                     ",
+}
+dashboard.section.buttons.val = {
+  dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
+  dashboard.button("e", " " .. " New file", ":ene <BAR> startinsert <CR>"),
+  dashboard.button(
+    "p",
+    " " .. " Find project",
+    ":lua require('telescope').extensions.projects.projects()<CR>"
+  ),
+  dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+  dashboard.button("t", " " .. " Find text", ":Telescope live_grep <CR>"),
+  dashboard.button("c", " " .. " Config", ":e ~/.config/nvim/init.lua <CR>"),
+  dashboard.button("q", " " .. " Quit", ":qa<CR>"),
+}
+local function footer()
+  -- NOTE: requires the fortune-mod package to work
+  -- local handle = io.popen("fortune")
+  -- local fortune = handle:read("*a")
+  -- handle:close()
+  -- return fortune
+  return "github.com/hhacker1999"
+end
+
+dashboard.section.footer.val = footer()
+
+dashboard.section.footer.opts.hl = "Type"
+dashboard.section.header.opts.hl = "Include"
+dashboard.section.buttons.opts.hl = "Keyword"
+
+dashboard.opts.opts.noautocmd = true
+alpha.setup(dashboard.opts)
